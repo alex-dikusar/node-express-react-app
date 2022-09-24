cf9491bc5cf7d4eeeb3fd09c9911cef613f12055
Alexander Dikusar <Alexander.Dikusar.contractor@pepsico.com>
2022-09-25 00:19:23 +0300

    add prettier and eslint, modify gitignore

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Template NodeJs + ExpressJS + ReactJS application
 
 ## Backend side
+
 1. Install Dependencies
    run `npm install`
 2. to run app use `npm start`
 
 ## Client side
+
 1. Install Dependencies
    run `npm install`
 2. to run app use `npm start`
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,16 +6,17 @@ const app: Express = express();
 const port = process.env.PORT;
 
 const startApp = async () => {
-    try {
-        await db.authenticate();
-        await db.sync();
-        app.listen(
-            port,
-            () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-        );
-    } catch (e) {
-        console.error(e);
-    }
-}
+  try {
+    await db.authenticate();
+    await db.sync();
+    app.listen(port, () =>
+      console.log(
+        `⚡️[server]: Server is running at https://localhost:${port}`,
+      ),
+    );
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 startApp();
