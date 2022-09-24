c08f298a9e8869609b14eeadd77b45ab240c16a3
Alexander Dikusar <Alexander.Dikusar.contractor@pepsico.com>
2022-09-24 23:46:41 +0300

    init server, and postgres with docker

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express } from 'express';
+import './env';
+import db from './db';
+
+const app: Express = express();
+const port = process.env.PORT;
+
+const startApp = async () => {
+    try {
+        await db.authenticate();
+        await db.sync();
+        app.listen(
+            port,
+            () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+        );
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+startApp();
